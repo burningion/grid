68bf750592b33151d7c817e3a33f20a026b3cad2
Andrew McWatters <mcwattersandrew@gmail.com>
2014-12-25 20:26:40 -0700

    Updated main view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,29 +11,24 @@ angular.module('publicApp')
   .controller('MainCtrl', function ($scope) {
     $scope.team = [
       {
+        name:   'Davlyn Hafaiedh',
+        latlng: '50.501079,4.4764595'
+      },
+      {
         name:   'Andrew McWatters',
-        latlng: '33.6054149,-112.125051',
-        role:   'Lead Software Architect, Lead Writer'
-      },
-      {
-        name:   'Kamil Zmich',
-        latlng: '53.3957166,-1.4994561',
-        role:   'Software Developer'
-      },
-      {
-        name:   'Davlyn',
-        latlng: '50.501079,4.4764595',
-        role:   'Lead Musician, Sound Engineer'
+        latlng: '33.6054149,-112.125051'
       },
       {
         name:   'Matthias Moninger',
-        latlng: '47.6964719,13.3457347',
-        role:   'Software Architect'
+        latlng: '47.6964719,13.3457347'
       },
       {
         name:   'Lachlan',
-        latlng: '-39.3080008,176.9160145',
-        role:   'Project Manager, Writer'
+        latlng: '-39.3080008,176.9160145'
+      },
+      {
+        name:   'Kamil Zmich',
+        latlng: '53.3957166,-1.4994561'
       }
     ];
   });
