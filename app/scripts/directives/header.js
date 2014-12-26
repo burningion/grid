f49b0f4cdc6782075b9a89bce3665bce761fffd9
Andrew McWatters <mcwattersandrew@gmail.com>
2014-12-25 20:52:40 -0700

    Disabled navigation

--- app/scripts/directives/header.js
+++ app/scripts/directives/header.js
@@ -27,32 +27,32 @@ angular.module('publicApp')
             // fade in header
             element.addClass('in');
 
-            // expand horizontal rule
-            element.find('hr').addClass('expand');
-
-            // initialize timeline and retrieve elements to animate
-            var tl = new TimelineMax();
-            var navLinks = element[0].querySelectorAll('li a');
-            var navTitle = $document[0].getElementById('nav-title');
-            tl
-              // fade in and slide down links
-              .staggerFromTo(navLinks, 0.4, {
-                  opacity: 0,
-                  y: -17 + -18
-                }, {
-                  opacity: 1,
-                  y: 0
-                }, 0.06)
-              // fade in and rotate in title
-              .fromTo(navTitle, 0.6, {
-                opacity: 0,
-                rotationX: 90,
-                transformOrigin: '50% 50% -8.5px'
-              }, {
-                opacity: 1,
-                rotationX: 0,
-                transformOrigin: '50% 50% -8.5px'
-              }, '-=0.4');
+            // // expand horizontal rule
+            // element.find('hr').addClass('expand');
+            //
+            // // initialize timeline and retrieve elements to animate
+            // var tl = new TimelineMax();
+            // var navLinks = element[0].querySelectorAll('li a');
+            // var navTitle = $document[0].getElementById('nav-title');
+            // tl
+            //   // fade in and slide down links
+            //   .staggerFromTo(navLinks, 0.4, {
+            //       opacity: 0,
+            //       y: -17 + -18
+            //     }, {
+            //       opacity: 1,
+            //       y: 0
+            //     }, 0.06)
+            //   // fade in and rotate in title
+            //   .fromTo(navTitle, 0.6, {
+            //     opacity: 0,
+            //     rotationX: 90,
+            //     transformOrigin: '50% 50% -8.5px'
+            //   }, {
+            //     opacity: 1,
+            //     rotationX: 0,
+            //     transformOrigin: '50% 50% -8.5px'
+            //   }, '-=0.4');
           }); });
         });
 
@@ -61,10 +61,10 @@ angular.module('publicApp')
          * Handle "viewLoaded" events.
          */
 
-        scope.$on('viewLoaded', function() {
-          var navTitle = $document[0].getElementById('nav-title');
-          navTitle.textContent = null;
-        });
+        // scope.$on('viewLoaded', function() {
+        //   var navTitle = $document[0].getElementById('nav-title');
+        //   navTitle.textContent = null;
+        // });
       }
     };
   });
