2e8a6c37d8bd99897eb8140e1058a977431a4301
Andrew McWatters <mcwattersandrew@gmail.com>
2015-01-17 15:55:16 -0700

    Added angular-sanitize to test/karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/fastclick/lib/fastclick.js',
       'bower_components/async/lib/async.js',
       'bower_components/gsap/src/uncompressed/TweenMax.js',
