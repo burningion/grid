84fbe8198bf6c9ecc7c8e9dd1f8858d43014ba43
Andrew McWatters <mcwattersandrew@gmail.com>
2015-02-03 20:26:32 -0700

    Updated test/karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/fastclick/lib/fastclick.js',
       'bower_components/async/lib/async.js',
