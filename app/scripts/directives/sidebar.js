808d1aae02c0fe4dd22e9f28fb94de7703d3c726
Andrew McWatters <mcwattersandrew@gmail.com>
2015-06-13 15:18:39 -0700

    Fixed JSHint issue in sidebar directive

--- app/scripts/directives/sidebar.js
+++ app/scripts/directives/sidebar.js
@@ -20,12 +20,6 @@ angular.module('publicApp')
     return {
       restrict: 'C',
       link: function postLink(scope, element) {
-        // Returns a random integer between min (included) and max (excluded)
-        // Using Math.round() will give you a non-uniform distribution!
-        function getRandomInt(min, max) {
-          return Math.floor(Math.random() * (max - min)) + min;
-        }
-
         /**
          * Restore CSS transitions on the next frame.
          */
